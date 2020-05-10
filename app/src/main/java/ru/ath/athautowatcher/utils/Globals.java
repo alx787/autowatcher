9085a21ce52dcd6a82769b6c2b1438c4298e9889
alex <kaanova@gmail.com>
2020-05-10 20:57:37 +0300

    tune track list view

--- app/src/main/java/ru/ath/athautowatcher/utils/Globals.java
+++ app/src/main/java/ru/ath/athautowatcher/utils/Globals.java
@@ -1,5 +1,5 @@
 package ru.ath.athautowatcher.utils;
 
 public class Globals {
-    public static final String MAPKIT_API_KEY = "8d3a1524-b690-4bc1-8777-35a040ffe18b";
+    public static final String MAPKIT_API_KEY = "";
 }
